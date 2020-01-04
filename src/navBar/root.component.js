2a4fb78c2a397dcaabc41386bd3a57361f56a1ac
giladd <giladd@trainologic.com>
2020-01-04 14:45:58 +0200

    Add custom event

--- src/navBar/root.component.js
+++ src/navBar/root.component.js
@@ -2,11 +2,26 @@ import React from "react";
 import { navigateToUrl } from "single-spa";
 
 const NavBar = () => {
+  const [count, setCount] = React.useState(0);
+
+  const handleCountChange = React.useCallback(
+    ev => {
+      setCount(ev.detail);
+    },
+    [setCount]
+  );
+
+  React.useEffect(() => {
+    window.addEventListener("reactApp:count:changed", handleCountChange);
+    return () =>
+      window.removeEventListener("reactApp:count:changed", handleCountChange);
+  }, [handleCountChange]);
+
   return (
     <nav>
       <div className="nav-wrapper">
         <a href="/" onClick={navigateToUrl} className="brand-logo">
-          single-spa demo
+          single-spa demo ({count})
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li>
--- src/react/root.component.js
+++ src/react/root.component.js
@@ -5,6 +5,15 @@ import _style from "./index.css";
 const App = () => {
   const [count, setCount] = React.useState(0);
 
+  React.useEffect(() => {
+    dispatchEvent(
+      new CustomEvent("reactApp:count:changed", {
+        detail: count,
+        bubbles: true
+      })
+    );
+  }, [count]);
+
   return (
     <div className="react-app-container">
       React App
